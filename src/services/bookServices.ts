0ef6e77766961ecb9044965c86796957571ec450
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-04-08 13:53:38 -0300

    fix: functions type

--- src/services/bookServices.ts
+++ src/services/bookServices.ts
@@ -1,21 +1,21 @@
 import errorsCategory from "../errors/index.js";
-import { CreateBook, CreateMyBook } from "../protocols/protocols";
+import { Book, CreateBook, CreateMyBook, MyBook } from "../protocols/protocols";
 import bookRepositories from "../repositories/bookRepositories.js";
 
-async function create({ name, author, userId } : CreateBook) {
+async function create({ name, author, userId } : CreateBook): Promise<void> {
   const {rowCount} = await bookRepositories.findByName({name});
   if (rowCount) throw errorsCategory.conflictError("Book already exists");
 
   await bookRepositories.create({name, author, userId});
 }
 
-async function findAll() {
+async function findAll(): Promise<Book[]>  {
   const books = await bookRepositories.findAll();
   if (!books.rowCount) throw errorsCategory.notFoundError();
   return books.rows;
 }
 
-async function takeBook({userId, bookId}: CreateMyBook) {
+async function takeBook({userId, bookId}: CreateMyBook): Promise<void> {
   const { rows: [book] } = await bookRepositories.findById({bookId});
   if (!book) throw errorsCategory.notFoundError();
   if(!book.available) throw errorsCategory.conflictError("Book not available");
@@ -24,7 +24,7 @@ async function takeBook({userId, bookId}: CreateMyBook) {
   await bookRepositories.takeBook({userId,bookId});
 }
 
-async function findAllMyBooks({userId}: Omit<CreateMyBook, "bookId">) {
+async function findAllMyBooks({userId}: Omit<CreateMyBook, "bookId">): Promise<MyBook[]> {
   const books = await bookRepositories.findAllMyBooks({userId});
   return books.rows;
 }
